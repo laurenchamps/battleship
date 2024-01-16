4e53dd6f040c443345975f781c8b51ddf1da47e0
Lauren Champs <laurenkchamps@gmail.com>
2024-01-16 11:07:41 +1030

    Create and test Gameboard and Player classes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
+/* 
+----- Ship types ------
+
+Carrier: 5
+Battleship: 4
+Cruiser: 3
+Submarine: 3
+Destroyer: 2 
+*/
+
 class Ship {
-    constructor(length, orientation) {
+    constructor(name, length) {
+        this.name = name;
         this.length = length;
-        this.orientation = orientation;
         this.hits = 0;
     }
 
@@ -15,6 +25,94 @@ class Ship {
     }
 }
 
+class GameBoard {
+    constructor(rows, columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.grid = [];
+        for (let i = 0; i < rows; i++) {
+            this.grid.push([]);
+            for (let j = 0; j < columns; j++) {
+                this.grid[i].push({
+                    shipName: null,
+                    attacked: null,
+                });
+            }
+        }
+        this.ships = [];
+    }
+
+    placeShip(row, column, length, name, isHorizontal) {
+        const ship = new Ship(name, length);
+
+        this.grid[row][column].shipName = name;
+
+        for (let i = 1; i < length; i++) {
+            if (isHorizontal) {
+                this.grid[row][column + i].shipName = name;
+            } else {
+                this.grid[row + i][column].shipName = name;
+            }
+        }
+
+        this.ships.push(ship);
+    }
+
+    receiveAttack(x, y) {
+        // Record attack on target coordinates
+        const target = this.grid[x][y];
+        target.attacked = true;
+        
+        // If no ship at target, record a miss
+        if (!target.shipName) return 'miss';
+
+        // Record a hit on the ship located at the target coords
+        this.ships.forEach(ship => {
+            if(ship.name == target.shipName) {
+                ship.hit();
+            }
+            // Check if ship is sunk and if so remove from ships array
+            if(ship.isSunk()) this.ships = this.ships.filter(ship => ship.name != target.shipName);
+        })
+            return 'hit';
+        }
+
+    hasActiveShips() {
+        if (this.ships.length) return true;
+        return false;
+    }
+}
+
+class Player {
+    constructor(name) {
+        this.name = name;
+    }
+
+    play(gameboard, x, y) {
+        // Valid plays only
+        // Coordinates must be within gameboard bounds
+        if (x < 0 || x >= gameboard.rows - 1 || y < 0 || y >= gameboard.columns - 1) return 'Target must be within the playing area.';
+
+        // Coordinates must not have previously received an attack
+        if (gameboard.grid[x][y].attacked) return 'Target has already been attacked. Choose another target.';
+
+        // Make play
+        gameboard.receiveAttack(x, y);
+    }
+
+    randomPlay(gameboard) {
+        // Generate valid random coordinates
+        let x, y;
+        // Generate random x and y coordinates within the gameboard grid
+        do {
+            x = Math.floor(Math.random() * gameboard.rows);
+            y = Math.floor(Math.random() * gameboard.columns);
+        } while (gameboard.grid[x][y].attacked);
+            
+        gameboard.receiveAttack(x, y);
+    }
+}
+
 export { Ship };
-
-
+export { GameBoard };
+export { Player };
--- src/app.test.js
+++ src/app.test.js
@@ -1,15 +1,65 @@
 import { Ship } from './app';
+import { GameBoard } from './app';
+import { Player } from './app';
+
+test('creates a ship', () => {
+    const ship = new Ship('cruiser', 3);
+    expect(ship).toEqual({name: 'cruiser', length: 3, hits: 0});
+})
 
 test('takes a hit', () => {
-    const ship = new Ship(3);
+    const ship = new Ship('cruiser', 3);
     ship.hit();
     expect(ship.hits).toEqual(1);
 })
 
 test('ship is sunk', () => {
-    const ship = new Ship(3);
+    const ship = new Ship('cruiser', 3);
     ship.hit();
     ship.hit();
     ship.hit();
     expect(ship.isSunk()).toBe(true);
 })
+
+test('creates blank gameboard', () => {
+    const gameBoard = new GameBoard(2, 2);
+    expect(gameBoard.grid).toEqual([[{attacked: null, shipName: null}, {attacked: null, shipName: null}], [{attacked: null, shipName: null}, {attacked: null, shipName: null}]]);
+})
+
+test('places ship on gameboard', () => {
+    const cruiser = new Ship('cruiser', 3);
+    const gameBoard = new GameBoard(10, 10);
+    gameBoard.placeShip(0, 0, cruiser.length, cruiser.name, true);
+    expect(gameBoard.grid[0][0]).toEqual( {attacked: null, shipName: 'cruiser'});
+})
+
+test('gameboard records location of attack', () => {
+    const gameBoard = new GameBoard(10, 10);
+    const cruiser = new Ship('cruiser', 3);
+    gameBoard.placeShip(0, 0, cruiser.length, cruiser.name, true);
+    gameBoard.receiveAttack(0, 0);
+    expect(gameBoard.grid[0][0].attacked).toEqual(true);
+})
+
+test('gameboard can report when all ships are sunk', () => {
+    const gameBoard = new GameBoard(10, 10);
+    const cruiser = new Ship('cruiser', 3);
+    gameBoard.placeShip(0, 0, cruiser.length, cruiser.name, true);
+    gameBoard.receiveAttack(0, 0);
+    expect(gameBoard.hasActiveShips()).toBe(true);
+    gameBoard.receiveAttack(0, 1);
+    gameBoard.receiveAttack(0, 2);
+    expect(gameBoard.hasActiveShips()).toBe(false);
+})
+
+test('randomly plays', () => {
+    const gameBoard = new GameBoard(10, 10);
+    const cruiser = new Ship('cruiser', 3);
+    const computer = new Player();
+    gameBoard.placeShip(0, 0, cruiser.length, cruiser.name, true);
+    for (let i = 0; i < 100; i++) {
+        computer.randomPlay(gameBoard);
+    }
+    expect(gameBoard.hasActiveShips()).toBe(false);
+})
+
